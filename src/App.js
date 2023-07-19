df6b12eeb3f3b5c5ec20a0d9d052046c8f0594e8
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-19 16:46:03 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./Style/Header.css";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Header = () => {
-  return <div></div>;
+const Header = ({ heading, subHeading }) => {
+  return (
+    <section className="header">
+      <h1 className="heading">{heading}</h1>
+      <p className="subHeading">{subHeading}</p>
+    </section>
+  );
 };
 
 export default Header;
