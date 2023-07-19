965dbc00daee9d41d4009bc3e2b9c1d118a6ceea
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-19 17:46:18 +0530

    Counter done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Counter = () => {
+  const [charlength, setCharLength] = useState(0);
+  const [wordLength, setWordLength] = useState(0);
+  const textRef = useRef();
+  const handleCount = () => {
+    const value = textRef.current.value;
+    setCharLength(value.length);
+    value.length
+      ? setWordLength(value.trim().split(" ").length)
+      : setWordLength(0);
+  };
+
+  const handleButton = () => {
+    textRef.current.value = "";
+    handleCount();
+  };
   return (
     <section className="counter">
       <textarea
+        onChange={handleCount}
+        ref={textRef}
         className="textArea"
         placeholder="Type or Paste your Text"
       ></textarea>
-      <button className="clearBtn">Clear</button>
+      <button onClick={handleButton} className="clearBtn">
+        Clear
+      </button>
       <p className="result">
-        <span>Characters: </span>
-        <span>Word: </span>
+        <span>Characters: {charlength}</span>
+        <span>Word: {wordLength}</span>
       </p>
     </section>
   );
--- src/pages/PageNotFound.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageNotFound = () => {
-  return <div></div>;
-};
-
-export default PageNotFound;
